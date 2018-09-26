3501b7a7a52b9c748ae03c0b5186213dfac49af7
ViktorVx <victorptrv@yandex.ru>
2018-09-26 16:07:08 +0300

    First commit

---
+++ src/main/java/com/pva/lessons/Lessons.java
@@ -0,0 +1,85 @@
+package com.pva.lessons;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Lessons {
+
+    public static int toBinary(int n) {
+        return Integer.valueOf(Integer.toBinaryString(n));
+    }
+
+    public static String stringy(int size) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            res.append(i%2==1 ? "0" : "1");
+        }
+        return res.toString();
+    }
+
+    public static String slope(int[] points) {
+        int dx = points[2] - points[0];
+        int dy = points[3] - points[1];
+        if (dx==0.0) {
+            return "undefined";
+        }
+        return String.valueOf(dy/dx);
+    }
+
+    public static int century(int number) {
+        return number%100==0 ? number/100 : number/100 +1;
+    }
+
+    public static int twiceAsOld(int dadYears, int sonYears){
+        int diff = dadYears - sonYears;
+        for (int i = 0; i < diff * 2; i++) {
+            if (i * 2 == diff) {
+                return Math.abs(diff - dadYears);
+            } else {
+                diff++;
+            }
+        }
+        return diff;
+    }
+
+    public static int[] invert(int[] array) {
+        return IntStream.of(array).map(n -> -n).toArray();
+
+    }
+
+    public static String fakeString(String numberString) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : numberString.toCharArray()) {
+            if (Character.getNumericValue(c) < 5) {
+                stringBuilder.append('0');
+            } else {
+                stringBuilder.append('1');
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static int grow(int[] x) {
+        return Arrays.stream(x).reduce((s1, s2) -> s1 * s2).orElse(0);
+    }
+
+    public static int[] map(int[] arr) {
+        return Arrays.stream(arr).map(num -> num *2).toArray();
+    }
+
+    public static int sum(int[] arr) {
+        return IntStream.of(arr).map(num -> num>0 ? num : 0).sum();
+    }
+
+    public static String seriesSum(int n) {
+        Double res = 0.0;
+        Double m = 1.0;
+        for (int i = 0; i < n; i++) {
+            res+=1.0/m;
+            m+=3.0;
+        }
+        return new BigDecimal(res).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
+    }
+}
---
+++ src/test/java/com/pva/lessons/LessonsTest.java
@@ -0,0 +1,110 @@
+package com.pva.lessons;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class LessonsTest {
+
+    @Test
+    public void toBinary() {
+        assertEquals(1, Lessons.toBinary(1));
+        assertEquals(10, Lessons.toBinary(2));
+        assertEquals(11, Lessons.toBinary(3));
+        assertEquals(101, Lessons.toBinary(5));
+    }
+
+    @Test
+    public void stringy() {
+        Random randGen = new Random();
+        for (int i = 0; i < 10; i++) {
+            int size = randGen.nextInt(50);
+            assertEquals("Wrong length using size " + size, size, Lessons.stringy(size).length());
+        }
+    }
+
+    @Test
+    public void slope() {
+        assertEquals("0",Lessons.slope(new int[]{19,3,20,3}));
+        assertEquals("undefined",Lessons.slope(new int[]{-7,2,-7,4}));
+        assertEquals("5",Lessons.slope(new int[]{10,50,30,150}));
+        assertEquals("-5",Lessons.slope(new int[]{15,45,12,60}));
+        assertEquals("6",Lessons.slope(new int[]{10,20,20,80}));
+        assertEquals("undefined",Lessons.slope(new int[]{-10,6,-10,3}));
+    }
+
+    @Test
+    public void century() {
+        assertEquals(18, Lessons.century(1705));
+        assertEquals(19, Lessons.century(1900));
+        assertEquals(17, Lessons.century(1601));
+        assertEquals(20, Lessons.century(2000));
+        assertEquals(1,  Lessons.century(89));
+    }
+
+    @Test
+    public void twiceAsOld() {
+        assertEquals(30, Lessons.twiceAsOld(30,0));
+        assertEquals(16, Lessons.twiceAsOld(30,7));
+        assertEquals(15, Lessons.twiceAsOld(45,30));
+    }
+
+    @Test
+    public void invert() {
+        int[] input = new int[] {-1,-2,-3,-4,-5};
+        int[] expected = new int[] {1,2,3,4,5};
+        assertEquals(Arrays.toString(expected), Arrays.toString(Lessons.invert(input)));
+
+        input = new int[] {-1,2,-3,4,-5};
+        expected = new int[] {1,-2,3,-4,5};
+        assertEquals(Arrays.toString(expected), Arrays.toString(Lessons.invert(input)));
+
+        input = new int[] {};
+        expected = new int[] {};
+        assertEquals(Arrays.toString(expected), Arrays.toString(Lessons.invert(input)));
+
+        input = new int[] {0};
+        expected = new int[] {0};
+        assertEquals(Arrays.toString(expected), Arrays.toString(Lessons.invert(input)));
+    }
+
+    @Test
+    public void fakeString() {
+        assertEquals("1110010010", Lessons.fakeString("8753493462"));
+    }
+
+    @Test
+    public void grow() {
+        assertEquals(6, Lessons.grow(new int[]{1,2,3}));
+        assertEquals(16, Lessons.grow(new int[]{4,1,1,1,4}));
+        assertEquals(64, Lessons.grow(new int[]{2,2,2,2,2,2}));
+    }
+
+    @Test
+    public void map() {
+        assertArrayEquals(new int[] {2, 4, 6}, Lessons.map(new int[] {1, 2, 3}));
+        assertArrayEquals(new int[] {8, 2, 2, 2, 8}, Lessons.map(new int[] {4, 1, 1, 1, 4}));
+        assertArrayEquals(new int[] {2, 2, 2, 2, 2, 2}, Lessons.map(new int[] {1, 1, 1, 1, 1, 1}));
+    }
+
+    @Test
+    public void sum() {
+        assertEquals(15, Lessons.sum(new int[]{1,2,3,4,5}));
+        assertEquals(13, Lessons.sum(new int[]{1,-2,3,4,5}));
+        assertEquals(0, Lessons.sum(new int[]{}));
+        assertEquals(0, Lessons.sum(new int[]{-1,-2,-3,-4,-5}));
+        assertEquals(9, Lessons.sum(new int[]{-1,2,3,4,-5}));
+    }
+
+    @Test
+    public void seriesSumTest() {
+        assertEquals("1.00", Lessons.seriesSum(1));
+        assertEquals("1.25", Lessons.seriesSum(2));
+        assertEquals("1.39", Lessons.seriesSum(3));
+        assertEquals("1.49", Lessons.seriesSum(4));
+        assertEquals("1.57", Lessons.seriesSum(5));
+    }
+}
