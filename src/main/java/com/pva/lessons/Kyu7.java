d8a7a374bec0eab95b86bc025046ce92eebc4548
ViktorVx <victorptrv@yandex.ru>
2018-09-29 12:59:34 +0300

    ReverseLetter2

--- src/main/java/com/pva/lessons/Kyu7.java
+++ src/main/java/com/pva/lessons/Kyu7.java
@@ -1,10 +1,8 @@
 package com.pva.lessons;
 
-import java.util.Collections;
-
 public class Kyu7 {
 
     public static String reverseLetter(final String str) {
-        return new StringBuilder(str).reverse().toString();
+        return new StringBuilder(str.replaceAll("[^a-zA-Z]", "")).reverse().toString();
     }
 }
--- src/test/java/com/pva/lessons/Kyu7Test.java
+++ src/test/java/com/pva/lessons/Kyu7Test.java
@@ -8,8 +8,13 @@ public class Kyu7Test {
 
     @Test
     public void reverseLetter() {
-        assertEquals("12345", Kyu7.reverseLetter("54321"));
-        assertEquals("54321", Kyu7.reverseLetter("12345"));
+        assertEquals("", Kyu7.reverseLetter("54321"));
+        assertEquals("", Kyu7.reverseLetter("12345"));
         assertEquals("ytrewq", Kyu7.reverseLetter("qwerty"));
+        assertEquals(Kyu7.reverseLetter("krishan"), "nahsirk");
+        assertEquals(Kyu7.reverseLetter("ultr53o?n"), "nortlu");
+        assertEquals(Kyu7.reverseLetter("ab23c"), "cba");
+        assertEquals(Kyu7.reverseLetter("krish21an"), "nahsirk");
+        assertEquals("zmpp[h]tqktdxaviqzizsssikkdqatde", Kyu7.reverseLetter("e4dtaqdkkiss%sz3izqivaxd|t=kqt]h_pp mz"));
     }
 }
