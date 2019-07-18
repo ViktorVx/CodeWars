fb0140ed3a65404732f230e665032f9347cb3f37
ViktorVx <victorptrv@yandex.ru>
2019-07-18 13:29:07 +0300

    Refactor Kyu2

--- src/test/java/com/pva/lessons/Kyu2Test.java
+++ src/test/java/com/pva/lessons/Kyu2Test.java
@@ -8,23 +8,17 @@ import static org.junit.Assert.assertEquals;
 
 public class Kyu2Test {
 
-//    private static Pattern regex = Pattern.compile(Kyu2.multipleOf7());
-    private static Pattern regex = Pattern.compile(Kyu2.multipleOf7Full());
-
     @Test
     public void EdgeCases() {
+        Pattern regex = Pattern.compile(Kyu2.multipleOf7Full());
         System.out.println("Testing for: empty string");
         assertEquals(false, regex.matcher("").matches());
         System.out.println("Testing for: 0");
         assertEquals(true, regex.matcher("0").matches());
-    }
 
-    @Test
-    public void FixedTests100() {
         for(int i=1; i<100; i++) {
             System.out.println("Testing for: "+i);
             assertEquals(i%7 == 0, regex.matcher(Integer.toBinaryString(i)).matches());
         }
     }
-
 }
