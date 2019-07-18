cc3497f109a9c12631d87d8337e4209d23080fe4
ViktorVx <victorptrv@yandex.ru>
2019-07-18 13:30:59 +0300

    Refactor Kyu3

--- src/main/java/com/pva/lessons/Kyu3.java
+++ src/main/java/com/pva/lessons/Kyu3.java
@@ -1,15 +1,5 @@
 package com.pva.lessons;
 
-
-import java.util.*;
-
 public class Kyu3 {
-
-    //********************************
-    public static Double evaluate(String expression) {
-        // your code here
-        return -1.0;
-    }
-    //********************************
 
 }
--- src/test/java/com/pva/lessons/Kyu3Test.java
+++ src/test/java/com/pva/lessons/Kyu3Test.java
@@ -1,12 +1,5 @@
 package com.pva.lessons;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 public class Kyu3Test {
 
-    @Test
-    public void evaluate() {
-    }
 }
