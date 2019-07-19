dc349d7ce16bf00e411276d9c2d3657fbddcac3a
ViktorVx <victorptrv@yandex.ru>
2019-07-19 10:58:03 +0300

    The Millionth Fibonacci Kata

--- src/main/java/com/pva/lessons/Kyu3.java
+++ src/main/java/com/pva/lessons/Kyu3.java
@@ -1,5 +1,60 @@
 package com.pva.lessons;
 
-public class Kyu3 {
+import java.math.BigInteger;
+
+class Kyu3 {
+
+    //*** The Millionth Fibonacci Kata *********************************************************************************
+
+    static BigInteger fib(BigInteger n) {
+        BigInteger[][] identity = new BigInteger[][]{{BigInteger.ONE, BigInteger.ZERO},{BigInteger.ZERO, BigInteger.ONE}};
+        BigInteger[][] self = new BigInteger[][]{{BigInteger.ONE, BigInteger.ONE},{BigInteger.ONE, BigInteger.ZERO}};
+
+        BigInteger[][] f = pow(self, n.compareTo(BigInteger.ZERO)<0 ? n.multiply(BigInteger.valueOf(-1)) : n, identity);
+
+        return f[0][1].multiply(((n.compareTo(BigInteger.ZERO)<0 && (n.mod(new BigInteger("2")).equals(BigInteger.ZERO))) ? BigInteger.valueOf(-1) : BigInteger.ONE));
+
+    }
+
+    private static BigInteger[][] matrixMultiply(BigInteger[][] a, BigInteger[][] b) {
+        int aRows = a.length;
+        int aColumns = a[0].length;
+        int bColumns = b[0].length;
+
+        BigInteger[][] c = new BigInteger[aRows][bColumns];
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < bColumns; j++) {
+                c[i][j] = BigInteger.ZERO;
+            }
+        }
+
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < bColumns; j++) {
+                for (int k = 0; k < aColumns; k++) {
+                    c[i][j] = c[i][j].add(a[i][k].multiply(b[k][j]));
+                }
+            }
+        }
+        return c;
+    }
+
+    private static BigInteger[][] pow(BigInteger[][] x, BigInteger n, BigInteger[][] i) {
+        if (n.equals(BigInteger.ZERO)) {
+            return i;
+        } else if (n.equals(BigInteger.ONE)) {
+            return x;
+        } else {
+            BigInteger[][] y = pow(x, n.divide(new BigInteger("2")), i);
+            y = matrixMultiply(y, y);
+            if (n.mod(new BigInteger("2")).equals(BigInteger.ONE)) {
+                y = matrixMultiply(x, y);
+            }
+            return y;
+        }
+    }
+
+    //******************************************************************************************************************
+
+
 
 }
--- src/test/java/com/pva/lessons/Kyu3Test.java
+++ src/test/java/com/pva/lessons/Kyu3Test.java
@@ -1,5 +1,70 @@
 package com.pva.lessons;
 
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
 public class Kyu3Test {
+
+    //*** Fibonacci ****************************************************************************************************
+    @Test
+    public void testFib0() {
+        testFib(0, 0);
+    }
+
+    @Test
+    public void testFib1() {
+        testFib(1, 1);
+    }
+
+    @Test
+    public void testFib2() {
+        testFib(1, 2);
+    }
+
+    @Test
+    public void testFib3() {
+        testFib(2, 3);
+    }
+
+    @Test
+    public void testFib4() {
+        testFib(3, 4);
+    }
+
+    @Test
+    public void testFib5() {
+        testFib(5, 5);
+    }
+
+    @Test
+    public void testFib6() {
+        testFib(8, 6);
+    }
+
+    @Test
+    public void testFib6m() {
+        testFib(-8, -6);
+    }
+
+    @Test
+    public void testFib43m() {
+        testFib(433494437, -43);
+    }
+
+    @Test
+    public void testFib10() {
+        testFib(55, 10);
+    }
+
+    private static void testFib(long expected, long input) {
+        BigInteger found;
+        found = Kyu3.fib(BigInteger.valueOf(input));
+        assertEquals(BigInteger.valueOf(expected), found);
+    }
+
+    //******************************************************************************************************************
 
 }
