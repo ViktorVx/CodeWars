d1f45153f0b6e8904cb22341790b683f61078757
ViktorVx <victorptrv@yandex.ru>
2019-07-19 12:59:48 +0300

    Java Hacking: Hijack a JVM (able to get list of java processes)

--- src/main/java/com/pva/lessons/Kyu1.java
+++ src/main/java/com/pva/lessons/Kyu1.java
@@ -1,5 +1,68 @@
 package com.pva.lessons;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Kyu1 {
+
+    //*** Java Hacking: Hijack a JVM ***********************************************************************************
+
+    public static class Buglary$Targer {
+        private static String securityCode;
+
+        public Buglary$Targer() {
+            securityCode = "777";
+        }
+    }
+
+    public static String guessSecurityCode(final String pid) throws IOException {
+        //***
+        /*
+        stack - хранит последовательность вызовов функций
+        heap - область памяти, которая хранит объекты
+        1) Искомая строка хранится в куче +++
+        2) ? Нужно сделать дамп кучи????
+         */
+        //***
+        Process p = null;
+        try {
+            p = Runtime.getRuntime().exec("jcmd -l");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(p);
+        StringBuffer sbInput = new StringBuffer();
+        BufferedReader brInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        String foundLine = "UNKNOWN";
+        try {
+            while ((line = brInput.readLine()) != null) {
+                if (line.contains(pid)){
+                    foundLine = line;
+                }
+                System.out.println(line);
+                sbInput.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        p.destroy();
+
+//        String result = foundLine.substring(0, foundLine.indexOf(" "));
+//        String result = foundLine;
+//        System.out.println(result);
+
+        return null;
+
+    }
+
+    //******************************************************************************************************************
 
 }
--- src/test/java/com/pva/lessons/Kyu1Test.java
+++ src/test/java/com/pva/lessons/Kyu1Test.java
@@ -1,5 +1,14 @@
 package com.pva.lessons;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class Kyu1Test {
 
+
+    @Test
+    public void guessSecurityCode() throws IOException {
+        Kyu1.guessSecurityCode("43");
+    }
 }
