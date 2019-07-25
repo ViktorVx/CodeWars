3ec91ab0c869ee6d8096c3b4394b5e7c69990a26
ViktorVx <victorptrv@yandex.ru>
2019-07-25 16:03:29 +0300

    Battleship field validator

--- src/main/java/com/pva/lessons/Kyu3.java
+++ src/main/java/com/pva/lessons/Kyu3.java
@@ -1,6 +1,7 @@
 package com.pva.lessons;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 class Kyu3 {
 
@@ -53,8 +54,77 @@ class Kyu3 {
         }
     }
 
-    //******************************************************************************************************************
+    //*** Battleship field validator ***********************************************************************************
 
+    enum Direction {
+        RIGHT, DOWN
+    }
 
+    public static boolean fieldValidator(int[][] field) {
+        int[] ships = new int[]{4, 3, 2, 1};
+        int[][] fieldCopy = Arrays.stream(field).map(int[]::clone).toArray(int[][]::new);
+        for (int i = 1; i < fieldCopy.length-1; i++) {
+            for (int j = 1; j < fieldCopy[0].length-1; j++) {
+                if (fieldCopy[i][j] == 1) {
+                    if (fieldCopy[i - 1][j - 1] == 1 ||
+                            fieldCopy[i - 1][j + 1] == 1 ||
+                            fieldCopy[i + 1][j + 1] == 1 ||
+                            fieldCopy[i + 1][j - 1] == 1 ) return false;
+                }
+            }
+        }
+        for (int i = 0; i < fieldCopy.length; i++) {
+            for (int j = 0; j < fieldCopy[0].length; j++) {
+                if (fieldCopy[i][j] == 1) {
+                    printField(fieldCopy);
+                    int len = findShipLen(i, j, fieldCopy, 1, Direction.RIGHT);
+                    if (len == 1) {
+                        len = findShipLen(i, j, fieldCopy, 1, Direction.DOWN);
+                    }
+                    if (len > 4) return false;
+
+                    ships[len - 1]--;
+
+                    fieldCopy[i][j] = -1;
+                }
+            }
+        }
+        for (int ship : ships) {
+            if (ship!=0) return false;
+        }
+        return true;
+    }
+
+    public static int findShipLen(int i, int j, int[][] field, int shipLen, Direction direction) {
+        switch (direction) {
+            case RIGHT:
+                if (i + 1 < field.length && field[i + 1][j] == 1) {
+                    field[i + 1][j] = -1;
+                    return findShipLen(i + 1, j, field, ++shipLen, direction);
+                } else {
+                    return shipLen;
+                }
+            case DOWN:
+                if (j + 1 < field[0].length && field[i][j + 1] == 1) {
+                    field[i][j + 1] = -1;
+                    return findShipLen(i, j + 1, field, ++shipLen, direction);
+                } else {
+                    return shipLen;
+                }
+        }
+        return -1;
+    }
+
+    private static void printField(int[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                System.out.print((field[i][j]==0 ? "0" : field[i][j]) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------");
+    }
+
+    //*** Battleship field validator ***********************************************************************************
 
 }
